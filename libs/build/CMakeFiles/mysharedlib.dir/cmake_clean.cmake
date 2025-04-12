09e9611315d3766e78eaaed576338603fd1a885a
goshan9 <pickup2legs@gmail.com>
2025-04-12 12:49:02 +0200

    changes

---
+++ libs/build/CMakeFiles/mysharedlib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mysharedlib.dir/functions.cpp.o"
+  "CMakeFiles/mysharedlib.dir/functions.cpp.o.d"
+  "libmysharedlib.a"
+  "libmysharedlib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mysharedlib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ libs/build/CMakeFiles/mysharedlib.dir/functions.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/mysharedlib.dir/functions.cpp.o: \
+ /home/goshan9/diabetic_diary/libs/functions.cpp \
+ /usr/include/stdc-predef.h
